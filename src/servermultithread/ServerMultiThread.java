1666889db2e5284756f1a540eedf6df804f06b97
hu.yongan <hu.yongan@TAVE-PC20.OSDBSESTO.local>
2021-03-04 14:04:42 +0100

    Receives an int from the client

--- src/servermultithread/ServerMultiThread.java
+++ src/servermultithread/ServerMultiThread.java
@@ -1,42 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servermultithread;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author pogliani.mattia
- */
 public class ServerMultiThread {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        int risultato = 0;
+        int richiesta = 0;
+        String r;
+        
         try {
-            ServerSocket server
-                    = new ServerSocket(5500);
+            ServerSocket server = new ServerSocket(5500);
             System.out.println("Server è attivo e in ascolto");
+            Socket client = server.accept();
+            System.out.println("Connessione ricevuta");
 
-            while (true) {
-                Socket client = server.accept();
-                System.out.println("connessione ricevuta, ora se la smazza il thread");
-                Thread Serverino
-                        = new Thread(new ServerThread(client));
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            r = in.readLine();
+            
+            while (richiesta>0) {
+                Thread Serverino = new Thread(new ServerThread(risultato, richiesta));
                 Serverino.start();
 
+                richiesta--;
             }
 
+            System.out.println("Il risultato è: "+risultato);
         } catch (IOException ex) {
             Logger.getLogger(ServerMultiThread.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    
+    public synchronized void somma() {
+        
+    }
 }
